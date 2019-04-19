b32947942d67611feae656b6f34a208a9a8de2c3
anjanragh <anjanragh@gmail.com>
2019-04-19 03:57:16 -0400

    Moved elastic init code to boot. Cleaned up load script. Added search endpoint to books

--- common/models/books.js
+++ common/models/books.js
@@ -1,5 +1,24 @@
 'use strict';
 
-module.exports = function(Books) {
-
+const esClient = require('../../lib/elasticsearch');
+module.exports = function (Books) {
+    Books.search = function(text) {
+        return new Promise((resolve, reject)=>{
+            esClient.search('book', 'bookInfo', text)
+            .then(resp=>{
+                resolve(resp)
+            })
+            .catch(err=>{
+                reject(err);
+            })
+        })
+      }
+      Books.remoteMethod (
+            'search',
+            {
+              http: {path: '/search', verb: 'get'},
+              accepts: {arg: 'text', type: 'string', required: true, http: { source: 'query' } },
+              returns: {arg: 'name', type: 'string'}
+            }
+        );
 };
---
+++ server/boot/initEs.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const esClient = require('../../lib/elasticsearch');
+
+module.exports = function(app) {
+    esClient.indexExists('book')
+    .then(resp=>{
+        // return esClient.initIndex('book')
+        return resp
+    })
+    .then(resp=>{
+        return esClient.initMapping('book', 'bookInfo')
+    })
+    .catch(err=>{
+        console.log("there is an error");
+        console.log(err);
+    }) 
+}; 
